7d372ec9bfbe39aa5798e5350e21f3f993184961
Leo Moura <soareslmoura@gmail.com>
2017-12-29 08:25:00 -0200

    Configuração do template do admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,8 +10,9 @@ class Page{
 	private $tpl;
 	private $options = [];
 	private $defaults = [
-		"data"=>[]
-
+			"header"=>true,
+			"footer"=>true,
+			"data"=>[]
 	];
 
 	public function __construct($opts = array(), $tpl_dir = "/ecommerce/views/"){
@@ -32,7 +33,7 @@ class Page{
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"]=== true)$this->tpl->draw("header");
 
 	}
 
@@ -57,7 +58,7 @@ class Page{
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if($this->options["footer"]=== true)$this->tpl->draw("footer");
 		
 	}
 
